14bde86ea6d9162c812695d860858ccf9d4f2baa
Jacob <kim4jc@mail.uc.edu>
2025-02-06 18:21:08 -0500

    added followercount files

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -2,8 +2,8 @@ let length;
 let width;
 
 function calculateArea(){
-    length = parseFloat(document.getElementById("length").value);
-    width = parseFloat(document.getElementById("width").value);
+    length = parseFloat(document.getElementById(`length`).value);
+    width = parseFloat(document.getElementById(`width`).value);
     let area = width * length;
-    document.getElementById("result").innerHTML = "The area of the rectangle is: ${area}";
+    document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
