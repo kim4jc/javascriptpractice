cd3aa81bc8a78d47a33a261e53c905bd13cacefb
Jacob <kim4jc@mail.uc.edu>
2025-02-06 21:17:27 -0500

    added product exp feedback

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -3,7 +3,7 @@ function submitFeedback(){
     const age = document.getElementById("age").value;
     const email = document.getElementById("email").value;
     const job = document.getElementById("job").value;
-    const designation = document.getElementById("designation");
+    const designation = document.getElementById("designation").value;
     const productType = document.getElementById("productType").value;
     const feedback = document.getElementById("feedbackText").value;
     document.getElementById('userName').innerHTML = username;
@@ -17,8 +17,27 @@ function submitFeedback(){
     alert('Thank you for your valuable feedback')
 }
 
+function submitProductFeedback(){
+    const productExperience = document.getElementById("experienceText").value;
+    const productType = document.getElementById("productType").value;
+    document.getElementById("userProductChoice").innerHTML = productType;
+    document.getElementById("userProductExp").innerHTML = productExperience;
+    document.getElementById("userExp").style.display = "block";
+    document.getElementById("userExpFeedback").style.display = "none";
+    alert("Thank you for submitting your product experience")
+}
+
 const submitButton = document.getElementById("submitBtn");
 submitButton.onclick = submitFeedback;
+
+const userExpBtn = document.getElementById("userExpBtn");
+userExpBtn.onclick = submitProductFeedback;
+
+document.addEventListener("keydown", function(event){
+    if(event.key === "Enter"){
+        submitProductFeedback();
+    }
+});
 
 document.addEventListener('keydown', function(event) {
     if (event.key === 'Enter') {
